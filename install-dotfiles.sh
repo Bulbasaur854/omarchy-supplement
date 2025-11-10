c1068bf63b7aa9da16629447446f5ef86370cd6f
Bul8a54ur <tairshechter1118@gmail.com>
2025-11-10 17:25:32 +0200

    initial commit

---
+++ install-dotfiles.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+ORIGINAL_DIR=$(pwd)
+REPO_URL="https://github.com/Bulbasaur854/dotfiles"
+REPO_NAME="dotfiles"
+
+is_stow_installed() {
+  pacman -Qi "stow" &> /dev/null
+}
+
+if ! is_stow_installed; then
+  echo "Install stow first"
+  exit 1
+fi
+
+cd ~
+
+# Check if repo already exists on system
+if [ -d "$REPO_NAME" ]; then
+  echo "Repository '$REPO_NAME' already exists. Skipping clone"
+else
+  git clone "$REPO_URL"
+fi
+
+# Check if the clone was successful
+if [ $? -eq 0 ]; then
+  echo "Removing old configs"
+  rm -rf ~/.config/backgrounds ~/.config/hypr/hyprlock.conf ~/.config/starship.toml ~/.bashrc
+
+  cd "$REPO_NAME"
+  stow backgrounds
+  stow bashrc
+  stow hyprlock
+  stow starship
+else
+  exho "Failed to clone the repository"
+  exit 1
+fi
