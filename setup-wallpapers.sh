0fe458ebc36e92afc0408e8e423dac4e5a43b767
Bul8a54ur <tairshechter1118@gmail.com>
2025-11-11 18:56:31 +0200

    setsid to run swaybg independently

--- setup-wallpapers.sh
+++ setup-wallpapers.sh
@@ -8,10 +8,10 @@ sleep 0.2
 MONITORS=($(hyprctl monitors -j | jq -r '.[].name'))
 
 if [ "${#MONITORS[@]}" -eq 1 ]; then
-  uwsm app -- swaybg \
+  uwsm app -- setsid swaybg \
     -o "${MONITORS[0]}" -i ~/.config/backgrounds/group_of_pink_flowers.jpg -m fill
 else
-  uwsm app -- swaybg \
+  uwsm app -- setsid swaybg \
     -o "${MONITORS[0]}" -i ~/.config/backgrounds/astronaut_playing_a_piano.jpg -m fill \
     -o "${MONITORS[1]}" -i ~/.config/backgrounds/group_of_pink_flowers.jpg -m fill
 fi
